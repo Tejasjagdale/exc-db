9a997f035e7b727f2ad95281765d71c4654961a8
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-06-13 05:23:29 +0530

    server.js try2

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,11 @@
-module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('MY_HEROKU_URL'),
-  app: { 
-    keys: env.array('APP_KEYS')
+module.exports = ({
+  env
+}) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+      auth: {
+          secret: env('ADMIN_JWT_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+      },
   },
-})
+});
