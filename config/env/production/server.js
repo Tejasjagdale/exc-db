1a31642e0930ca9c4c9eb4c34eaaf7b6041c44b4
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-06-13 03:14:44 +0530

    db and server file changes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'thisisaapiappokgotit'),
+    },
   },
-});
+})
