976e8dcdd5bb2673bf605cdf6602f28e7181a940
Tejasjagdale <69722269+Tejasjagdale@users.noreply.github.com>
2022-06-13 05:31:43 +0530

    yarn update

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,9 @@
-module.exports = ({
-  env
-}) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
-      auth: {
-          secret: env('ADMIN_JWT_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
-      },
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+    },
   },
 });
